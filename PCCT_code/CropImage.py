fd0161759d81c0303d4794e5ee1a00ae244d8b78
Jilmen Quintiens <jilmen.quintiens@kuleuven.be>
2023-03-09 13:00:59 +0100

    Enhanced such that multiple ROIs can be loaded without reloading the image every time

--- PCCT_code/CropImage.py
+++ PCCT_code/CropImage.py
@@ -9,6 +9,7 @@ import SimpleITK as sitk
 import os
 import numpy as np
 import argparse
+import sys
 
 def CropImage(image, roi, margin, WRITE_IMAGE = False, outIm = '', outROI = ''):
     """
@@ -58,45 +59,72 @@ def main():
                                                    'Maximum positions in the mask are determined, and a margin is included.', \
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-image', help='input image to be cropped', required = True)
-    parser.add_argument('-mask', help='region of interest. Dimensions should match the image.', required = True)
+    parser.add_argument('-mask', help='region of interest. Dimensions should match the image. You can enter multiple ROIs, but give the same amount of output names', nargs='+')
     parser.add_argument('-margin', help='Margin beyond the extrema of the region of interest to crop. Should be specified in milimeters.', default=10)
     parser.add_argument('-outImg', help='Name of output image. You can choose to specify a full path or just a filename.'\
-                                        'In case of the latter, the files will be written in the same folder as the inputs', required = True)
+                                        'In case of the latter, the files will be written in the same folder as the inputs', nargs='+')
     parser.add_argument('-outMask', help='Name of output mask. You can choose to specify a full path or just a filename.'\
-                                        'In case of the latter, the files will be written in the same folder as the inputs', required = True)
+                                        'In case of the latter, the files will be written in the same folder as the inputs', nargs='+')
     args = parser.parse_args()
     
-    if not os.path.isfile(args.image) or not os.path.isfile(args.mask):
-        print('ERROR: you did not specify a valid image or mask file.')
+    # check if input image exists
+    if not os.path.exists(args.image):
+        print('ERROR: you did not specify a valid image')
+        sys.exit()
+        
+    #base strings of image and mask
+    pos = args.image.rfind('/')
+    base_im = args.image[:pos+1]
+    
+    # if one ROI is given, put in tuple for uniform handling
+    if not isinstance(args.mask, list) and not isinstance(args.outMask, list) and not isinstance(args.outImg, list):
+        set_masks = [args.mask]
+        set_outMasks = [args.outMask]
+        set_outImgs = [args.outImg]
+    
     else:
-        if args.outImg.find('/') == -1:
-            # image output has to be stored in same folder as input image
-            pos = args.image.rfind('/')
-            outImg = args.image[:pos+1] + args.outImg
-        else: 
-            # image output is explicitly given
-            outImg = args.outImg
+        if len(args.mask) != len(args.outMask) or len(args.mask) != len(args.outImg):
+            print('ERROR: you specified a list with multiple ROIs, but not the same number of output names.')
+            sys.exit()
+        else:
+            set_masks = args.mask
+            set_outMasks = args.outMask
+            set_outImgs = args.outImg
+            print(f'{len(set_masks)} mask files are given as input')
+            
+    print('Reading image...')
+    img = sitk.ReadImage(args.image)
+    
+    for mask_nb in range(len(set_masks)):
+        mask = set_masks[mask_nb]
+        outMask = set_outMasks[mask_nb]
+        outImg = set_outImgs[mask_nb]
+        print(f'mask {mask_nb+1}/{len(set_masks)}')
         
-        if args.outMask.find('/') == -1:
-            # mask output has to be stored in same folder as input mask
-            pos = args.mask.rfind('/')
-            outMask = args.mask[:pos+1] + args.outMask
-        else: 
-            # mask output is explicitly given
-            outMask = args.outMask
-        
-        # check if extension is explicitly mentioned
-        if outImg.find('.') == -1:
-            outImg += '.mha'
-        if outMask.find('.') == -1:
-            outMask += '.mha'
-        
-        print('Reading image...')
-        img = sitk.ReadImage(args.image)
-        print('Reading mask...')
-        mask = sitk.ReadImage(args.mask)
-        
-        CropImage(img, mask, args.margin, WRITE_IMAGE = True, outIm = outImg, outROI = outMask)
+    
+        if not os.path.isfile(mask):
+            print(f'{mask} is not a valid file. Skipping this mask.')
+            
+        else:
+            if outImg.find('/') == -1:
+                # image output has to be stored in same folder as input image
+                outImg = base_im + outImg
+                
+            if outMask.find('/') == -1:
+                # mask output has to be stored in same folder as input image
+                pos = mask.rfind('/')
+                outMask = mask[:pos+1] + outMask
+            
+            # check if extension is explicitly mentioned
+            if outImg.find('.') == -1:
+                outImg += '.mha'
+            if outMask.find('.') == -1:
+                outMask += '.mha'
+
+            print('Reading mask...')
+            mask = sitk.ReadImage(mask)
+            
+            CropImage(img, mask, float(args.margin), WRITE_IMAGE = True, outIm = outImg, outROI = outMask)
 
 if __name__ == '__main__':
     main()
